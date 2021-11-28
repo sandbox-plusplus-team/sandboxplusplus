1e91c8daacbc710e972a884feaf0e3a241554564
Ian Earley <gmanchewing@gmail.com>
2021-11-28 12:42:59 -0500

    Toolgun Particle Fix

--- code/tools/Tool.Effects.cs
+++ code/tools/Tool.Effects.cs
@@ -5,9 +5,16 @@ public partial class Tool
 	[ClientRpc]
 	public void CreateHitEffects( Vector3 hitPos )
 	{
-		var particle = Particles.Create("particles/physgun_freeze.vpcf", hitPos );
+		var particle = Particles.Create( "particles/tool_select_hit.vpcf", hitPos );
 		particle.SetPosition( 0, hitPos );
 		particle.Destroy( false );
+
+		var beam = Particles.Create( "particles/tool_tracer.vpcf", hitPos );
+		beam.SetEntityAttachment( 0, EffectEntity, "muzzle", true );
+		beam.SetPosition( 1, hitPos );
+
+		var particle2 = Particles.Create( "particles/tool_hit.vpcf", EffectEntity, "muzzle" ); ;
+		particle2.Destroy( false );
 
 		ViewModelEntity?.SetAnimBool("fire", true);
 
